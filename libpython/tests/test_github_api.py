dd682901b7fd251c672b65a72df9cc092e87d6c8
jfobatista <42Glsjhsrtljoao!>
2022-10-31 17:32:20 -0300

    Utilizando a lib pytest-mock para controlar o valor da get de request e utilizar o valor de mock quando necessário close #42

--- libpython/tests/test_github_api.py
+++ libpython/tests/test_github_api.py
@@ -4,17 +4,16 @@ from libpython import github_api
 
 
 @pytest.fixture
-def avatar_url():
+def avatar_url(mocker):
     resp_mock = Mock()
     url = "https://avatars.githubusercontent.com/u/99755055?v=4"
     resp_mock.json.return_value = {
         "login": "jfobatista", "id": 99755055, "node_id": "U_kgDOBfIkLw",
         "avatar_url": url
     }
-    get_original = github_api.requests.get
-    github_api.requests.get = Mock(return_value=resp_mock)
-    yield url
-    github_api.requests.get = get_original
+    get_mock = mocker.patch('libpython.github_api.requests.get')
+    get_mock.return_value = resp_mock
+    return url
 
 
 def test_buscar_avatar(avatar_url):
@@ -22,8 +21,6 @@ def test_buscar_avatar(avatar_url):
     assert avatar_url == url
 
 
-# foi necessario salvar o get original para termos o resultado real do get feito integralmente,
-# assim sendo possível realizar o mock e não perdermos o resultado resultado original do mesmo
 def test_buscar_avatar_integracao():
     url = github_api.buscar_avatar('jfobatista')
     assert 'https://avatars.githubusercontent.com/u/99755055?v=4' == url
